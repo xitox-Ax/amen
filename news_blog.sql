f2d488becc77e0e2a3b9d156cb31d2a127e57f90
xitox-Ax <xadl3287@gmail.com>
2025-04-21 10:46:24 +0200

    modified:   news_blog.sql

--- news_blog.sql
+++ news_blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 20, 2025 at 04:12 PM
+-- Generation Time: Apr 21, 2025 at 10:45 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -38,7 +38,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `password`) VALUES
-(1, 'admin', '482c811da5d5b4bc6d497ffa98491e38');
+(1, 'admin', '482c811da5d5b4bc6d497ffa98491e38'),
+(8, 'Amen', 'b55d6c402fe78f2be63114c6638aa9c1');
 
 -- --------------------------------------------------------
 
@@ -60,11 +61,7 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `title`, `content`, `image_url`, `date_posted`, `author`) VALUES
-(1, 'Nature', 'This image captures the serene beauty of nature. The lush greenery and the tranquil atmosphere make it a perfect escape from the hustle and bustle of city life. Nature has a way of calming our minds and rejuvenating our spirits plus.', 'imgs/nature.jpg', '2025-01-28', 'Amen'),
-(2, 'Environment ', 'This image captures the serene beauty of nature. The lush greenery and the tranquil atmosphere make it a perfect escape from the hustle and bustle of city life. Nature has a way of calming our minds and rejuvenating our spirits.', 'imgs/nature.jpg', '2025-01-28', 'Amen'),
-(14, 'Environment ', 'This image captures the serene beauty of nature. The lush greenery and the tranquil atmosphere make it a perfect escape from the hustle and bustle of city life. Nature has a way of calming our minds and rejuvenating our spirits.', 'imgs/nature.jpg', '2025-01-28', 'Amen'),
-(15, 'Environment ', 'This image captures the serene beauty of nature. The lush greenery and the tranquil atmosphere make it a perfect escape from the hustle and bustle of city life. Nature has a way of calming our minds and rejuvenating our spirits.', 'imgs/nature.jpg', '2025-01-28', 'Amen'),
-(16, 'Environment ', 'This image captures the serene beauty of nature. The lush greenery and the tranquil atmosphere make it a perfect escape from the hustle and bustle of city life. Nature has a way of calming our minds and rejuvenating our spirits.', 'imgs/nature.jpg', '2025-01-28', 'Amen');
+(22, 'Health and Beauty ', '<p>In Harare, healthcare&nbsp;refers to the services provided to maintain and improve people&#39;s health, encompassing prevention, diagnosis, treatment, and rehabilitation.&nbsp;It includes primary care, secondary care, and public health initiatives.&nbsp;Zimbabwe&#39;s healthcare system, while once a model, has faced challenges in recent decades, impacting the availability and quality of services.&nbsp;</p>\r\n\r\n<p>Here&#39;s a more detailed look at healthcare in Harare:</p>\r\n\r\n<p><strong>Key Aspects of Healthcare:</strong></p>\r\n\r\n<ul>\r\n	<li>\r\n	<p><strong>Diagnosis and Treatment:</strong></p>\r\n\r\n	<p>Involves identifying and treating illnesses and injuries through medical tests, consultations with doctors and specialists, and various therapies.</p>\r\n	</li>\r\n</ul>\r\n\r\n<ul>\r\n	<li>\r\n	<p><strong>Rehabilitation:</strong></p>\r\n\r\n	<p>Helps individuals recover from illnesses or injuries through therapies like physiotherapy, occupational therapy, and speech therapy.</p>\r\n	</li>\r\n</ul>\r\n\r\n<ul>\r\n	<li>\r\n	<p><strong>Public Health:</strong></p>\r\n\r\n	<p>Concerned with the health of the population as a whole, addressing issues like sanitation, water quality, and infectious disease control.&nbsp;</p>\r\n	</li>\r\n</ul>\r\n\r\n<ul>\r\n	<li>\r\n	<p><strong>Prevention:</strong></p>\r\n\r\n	<p>Focuses on promoting healthy lifestyles and preventing diseases through vaccinations, public health campaigns, and education.</p>\r\n	</li>\r\n</ul>\r\n', 'imgs/14.jpg', '2025-04-21', 'Allen C ');
 
 --
 -- Indexes for dumped tables
@@ -91,13 +88,13 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
